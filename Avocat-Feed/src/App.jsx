0c4827e75953ae165751d0c65fc36bb6e653b810
luaanaapereiraa <luanapdsantos@gmail.com>
2022-11-22 00:47:31 -0300

    Feat: Structure

---
+++ Avocat-Feed/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Header } from './components/Header'
+
+import { Post } from './Post'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Post />
+    </>
+  )
+}
+
+export default App
